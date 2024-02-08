8d70974a770d6b66e74c897833d61ed2b701eb00
vtecek1 <128639826+vtecek1@users.noreply.github.com>
2024-02-09 12:17:58 +1300

    Add files via upload

---
+++ throttlePico.py
@@ -0,0 +1,40 @@
+import time
+import machine
+from machine import ADC
+import utime
+ 
+
+ 
+mode = 'fsddrag'
+brake = True
+throttleInput = 10
+
+while True:
+    
+    adc = ADC(27)
+    throttleInput = adc.read_uv()
+    print(throttleInput)
+    
+    
+    while mode == 'drag':
+        if throttleInput >= 10 and brake == True:
+            throttleOutput = 25
+        elif throttleInput >= 10:
+            throttleOutput = 100
+        else:
+            throttleOutput = 0
+        #print(throttleOutput)
+        
+    while mode == 'race':
+        if throttleInput > 90:
+            throttleOutput = 100
+        elif throttleInput >= 10:
+            throttleOutput = throttleInput*1.1
+        else:
+            throttleOutput = 0
+        
+    while mode == 'eco':
+        previous = throttleInput
+        time.sleep(0.3)
+        throttleOutput = (previous + throttleInput)/2   
+        print('Throttle In: ' + throttleInput + 'Throttle In: ' + throttleOutput)
